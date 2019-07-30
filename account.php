cc6a11939c63b1e89a855b2495d91e65bf979ecb
Grant Upson <gupson@utas.edu.au>
2019-07-30 16:40:45 +1000

    Very untested stuff
    Harrys work, tried to clean it up, its static atm and doesnt do anything currently and needs to be cleaned.

--- account.php
+++ account.php
@@ -11,9 +11,149 @@
 ?>
 
 <article>
-  
-<!-- CODE HERE -->
+   <!--<aside id="account-panel">    DO NOT USE, NEED A PANEL THAT IS IN THE ARTICLE AND ABOVE OTHER ELEMENTS
+        <div class="username">
+            <a>User Name</a>
+            <hr>
+            <h2>ADMIN</h2>
 
+
+        </div>
+        <div class="list">
+            <a class="current"><i class="fa fa-address-card"></i> Dashboard</a>
+            <a><i class="fa fa-user-circle"></i> Profile</a>
+            <a>Club Profile</a>
+            <a>Players</a>
+            <a>Events</a>
+            <a>Settings</a>
+        </div>
+    </aside> -->
+    
+    <div class="recent-event">
+        <div class="recent-club">
+            <a>Your Club: Club Name</a>
+            <a>Players:20</a>
+            <a>Event:20</a>
+            <a>Director:10</a>
+            <button class="buton" style="background-color:#1FC498;">Add New Player</button>
+            <button class="buton" style="background-color:#FC429B;">Add New Event</button>
+            <button class="buton" style="background-color:#3AAAFE;">Add New Director</button>
+        </div>
+        <div class="heading">
+            <a>Recent Players</a>
+            <div class="recent-search">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+
+        <div class="tbl-header">
+            <table id="tbl-header-rows" cellpadding="0" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Event</th>
+                        <th>Club</th>
+                        <th>Date</th>
+                        <th>Sport</th>
+
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="tbl-content">
+            <table id="tbl-content-rows" cellpadding="0" cellspacing="0">
+                <tbody>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+
+                    </tr>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+                    </tr>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+                    </tr>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+                    </tr>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+                    </tr>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+                    </tr>
+                    <tr>
+                        <td>Event Name</td>
+                        <td>AUSTRALIAN </td>
+                        <td>25/07/2019</td>
+                        <td>Tennis</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="club-player">
+        <div class="heading">
+            <a>Club Player</a>
+            <div class="club-search">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+        <div class="detail">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>E-mail</th>
+                        <th>ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Nick Pettit</td>
+                        <td>nick@example.com</td>
+                        <td>23423</td>
+                    </tr>
+                    <tr>
+                        <td>Andrew Chalkley</td>
+                        <td>andrew@example.com</td>
+                        <td>31413</td>
+                    </tr>
+                    <tr>
+                        <td>Dave McFarland</td>
+                        <td>dave@example.com</td>
+                        <td>123112</td>
+                    </tr>
+                    <tr>
+                        <td>Guil Hernandez</td>
+                        <td>guil@example.com</td>
+                        <td>123112</td>
+                    </tr>
+
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 </article>
 
 <?php
--- players.php
+++ players.php
@@ -6,58 +6,93 @@
 ?>
 
 <article>
+    <div class="boxed">
+        <h2>Search players</h2>
+        <div class="vl"></div>
+        <div class="container">
+            <input type="text" id="fname" name="firstname" placeholder="Full Name">
+        </div>
+        <div class="sports">
+            <select id="sports" name="sports">
+                            <option value="Sports">Country</option>
+                            <option value="Tennis">Australia</option>
+                            <option value="Squash">New Zealand </option>
+                            <option value="Badminton">America</option>
+                        </select>
+        </div>
+        <div class="Region">
+            <select id="region" name="region">
+                            <option value="Region">state</option>
+                            <option value="Tasmania">Tasmania</option>
+                            <option value="Melbourne">Melbourne</option>
+                            <option value="Sydney">Sydney</option>
+                        </select>
+        </div>
+        <div class="Club">
+            <input type="text" placeholder="Club" name="search">
+            <button type="submit"><i class="fa fa-search"></i></button>
+        </div>
+        <div class="age">
+            <h1>Players Age:</h1>
+        </div>
+        <div class="container1">
+            <input type="text" id="fname" name="firstname" placeholder="Last Competitor">
+        </div>
+        <div class="tab">
+            <h3>Recently:</h3>
+            <button class="tablinks">7 Days</button>
+            <button class="tablinks">30 Days</button>
+            <button class="tablinks">180 Days</button>
+        </div>
 
-	<div class="player-search-filter-container">  
-		<form action="process-player-list.php" method="post">
-			<input type="text" name="player-name" class="" placeholder="Enter Player Name">
+        <div class="tab2">
+            <button class="tablink" id="clean" onclick=""><i class="fa fa-trash"></i> Clean</button>
+        </div>
+        <div class="tab3">
+            <button class="tablink" id="clean" onclick="">Search</button>
+        </div>
+        <div id="slider"></div>
+    </div>
 
-			<input type="text" name="club-name" class="" placeholder="Enter Club Name">
 
-			<select name="country-name" class="" placeholder="Select Country">
-				<option>Select Country</option>
-			</select>
+    <table class="paleBlueRows">
+        <thead>
+            <tr>
+                <th>Ranking</th>
+                <th>Player Name</th>
+                <th>Club</th>
+                <th>Region</th>
+                <th>Last Played</th>
+                <th>Last competitor</th>
+                <th>Age</th>
+          
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>YUSUF</td>
+                <td>Club Name</td>
+                <td>VIC,AU</td>
+                <td>20 July 2018</td>
+                <td>Adib</td>
+                <td>20</td>
+                
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>YUSUF</td>
+                <td>Club Name</td>
+                <td>VIC,AU</td>
+                <td>20 July 2018</td>
+                <td>Adib</td>
+                <td>20</td>
+                
+            </tr>
+        </tbody>
+    </table>
 
-			<select name="state-name" class="" placeholder="Select State">
-				<option>Select State</option>
-			</select>
-
-			<input type="text" name="recent-competitor" class="" placeholder="Most Recent Competitor">
-
-			<button type="submit" name="submit-search-filter" class="">Search</button>
-		</form>
-	</div>
-
-	<div class="player-search-result-container">
-		<table class="player-search-result-table">
-			<?php
-				if(isset($_SESSION["player-name"]) && $_SESSION["player-name"] != NULL)
-				{
-					$player = $_SESSION["player-name"];
-					$club = $_SESSION["club"];
-
-					echo "<tr class='player-search-result-table-headers'>
-							<th>Player</th>
-							<th>Club</th>
-						  </tr>";
-					
-					for($i = 0; $i < count($player); $i++)
-					{
-						echo "<tr>";
-						echo "<td><a href='profile.php'>".$player[$i]."</a></td>";
-						echo "<td>".$club[$i]."</td>";
-						echo "</tr>";
-					}					
-				}
-				else
-				{ 
-					echo "No player by the given filter exists.";
-				}
-
-				unset($_SESSION["player-name"]);
-				unset($_SESSION["club"]);
-			?>
-		</table>
-	</div>
+<script src="./javascript/nouislider.min.js"></script> 
 
 </article>
 
