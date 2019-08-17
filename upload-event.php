697d39c60b0af47d4471893faa0ef059d11cffe6
James <watkinsj@utas.edu.au>
2019-08-17 10:05:03 +1000

    removed hard coded popups
    Harry had hard coded popups in the upload-event.php but it was designed by adib to be included so easily changed.

--- upload-event.php
+++ upload-event.php
@@ -86,75 +86,11 @@
 
 <!-- advanced search -->
 
-<div class="player-advanced-search-border">
-    <div class="advanced-search-content">
-      <div class="player-advanced-search-wrapper">
-          <h2>Advanced player search</h2>
-          <div class="advanced-player-exit-button" onclick="hideAdvancedSearchModal()">+</div>
-  	   </div>
-  <div class="input-advanced-player-name-wrapper">
-       <input type="text" id="input-player-name" name="given-name" placeholder="Player Name" pattern="[a-zA-Z\s]{1,45}" required="" title="Player name must be within 1-45 characters"><br>
-     </div>
-  </div>
-</div>
+<?php include("./includes/advancedPlayerSearch.php"); ?>
 
 <!-- add player -->
 
-<div class="add-player-border">
-      <div class="player-content">
-      <div class="player container">
-      <div class="advanced-player-search-wrapper">
-          
-          <div class="add-player-exit-button" onclick="hideAddPlayerModal()">+</div>
-  	   </div>
-  
-     <div class="add-player-wrapper">
-        <div class="add-player-header">
-          <h2>Add Player</h2>
-        </div>
-
-        <hr>
-        <!-- form for uploading excel file -->
-        <form method="post" action="" enctype="multipart/form-data">
-          <input type="file" class="player-file" name="player-file">
-          <input type="submit" id="add-player-file" name="add-player-file" value="Add File">
-        </form>
-
-        <form method="post">
-
-        <div class="add-player-content">
-          <input type="text" id="player-given-name" name="given-name" placeholder="Given Name" pattern="[a-zA-Z\s]{1,45}" required="" title="Given name must be within 1-45 characters">
-          <input type="text" id="player-family-name" name="family-name" placeholder="Family Name" pattern="[a-zA-Z\s]{1,45}" required="" title="Family name must be within 1-45 characters">
-        </div>
-          
-        <div class="add-player-content">
-           <select class="player-gender" name="player-gender" id="player-gender-ID">
-              <option value="M">Male</option>
-              <option value="F">Female</option>
-            
-          </select>
-        <input name="player-birth-date" id="player-birth-date" placeholder="DOB" required="" type="text" onfocus="(this.type='date')" onblur="(this.type='text')">
-         
-          
-          </div>
-          
-          <div class="add-player-content">
-            <input type="email" id="player-email" name="player-email" placeholder="Email" pattern="{7,75}" required="" title="Email must not exceed 75 characters"> 
-            
-             <select class="player-club" name="player-club-name" id="player-club-ID">
-              <option value="1">Launceston Badminton Club</option><option value="2">Otago Squash Club</option>            </select>
-          </div>
-          
-          <br>
-
-          <button type="button" name="add-player-button" id="add-player-button" onclick="addPlayer()">Add Player</button>
-        </form>
-
-        </div>
-    </div>
-</div>
-</div>
-
+<?php include("./includes/add-player.php"); ?>
 
 <!-- initial rating -->
 <?php include("./includes/initialRating.php"); ?>
