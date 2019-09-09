005fa95b12f5b2e79a6c46e09d225af4ef489e3a
harry <3286.harinder@gmail.com>
2019-09-09 18:36:55 +1000

    added a missing hr line after notification heading

--- includes/event-type-notification-modal.php
+++ includes/event-type-notification-modal.php
@@ -1,21 +1,22 @@
-<!-- Harrinder's work for notification modal -->
+<!-- Harinder's work for notification modal -->
 
 <div class="event-type-notification-modal-background">
   <div class="event-type-notification-modal-content">
 
-  	<div class="event-type-notification-modal-header">
-  	  <div class="event-type-notification-modal-exit-button" onclick="hideTypeModal()">+</div>
-  	</div>
+    <div class="event-type-notification-modal-header">
+      <div class="event-type-notification-modal-exit-button" onclick="hideTypeModal()">+</div>
+    </div>
 
-  	<div class="event-type-notification-modal-fields">
-  	  <h2 id="notification-header-text">Notification</h2>
+    <div class="event-type-notification-modal-fields">
+      <h2 id="notification-header-text">Notification</h2>
+      <hr/>
   
       <div class="event-type-notification-modal-field-wrapper">
           <p id="notification-modal-text"></p>
       </div>
-  	</div>
-	<button type = "submit" class="change-match-button" value="Yes" onclick="changeType()">Yes</button>
-	<button type = "submit" class="change-match-button" value="No" onclick="hideTypeModal()">No</button>
+    </div>
+  <button type = "submit" class="change-match-button" value="Yes" onclick="changeType()">Yes</button>
+  <button type = "submit" class="change-match-button" value="No" onclick="hideTypeModal()">No</button>
   </div>
 </div>
 
