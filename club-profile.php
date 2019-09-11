a038235fa79c4c2fbe3942e05aeb290737f05ead
James <watkinsj@utas.edu.au>
2019-09-11 15:30:13 +1000

    everything except css

---
+++ club-profile.php
@@ -0,0 +1,174 @@
+<?php 
+    $title = "Peterman Ratings | Club-profile";
+
+    include("./includes/header.php");
+    include("./includes/navigation.php");
+?>
+
+<article id="club-profile-page-article">
+  <div class="clubs-information-container">
+      <h1>Habart Badminton Club</h1>
+      <h2>Badminton</h2>
+      <h2>Tasmania</h2>
+      <h2>Australia</h2> 
+  </div>
+  <div class="club-members-container">
+      <h2>Club Members List</h2>
+      <table class='club-members-table'>
+	      <tr>
+	      <th>Player</th>
+	      <th>Age</th>
+	      <th>Last Played</th>
+	      </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+          <tr>
+          <td>PlayName</td>
+          <td>28</td>
+          <td>19 August 2019</td>
+          </tr>
+        </table>
+    
+  </div>
+    
+  <div class="search-pagination-buttons">
+        <span class="player-search-link player-search-link-active" id="1">&lt;&lt;</span>
+        <span class="player-search-link player-search-link-active" id="1">1 </span>
+        <span class="player-search-link player-search-link-active" id="2">2 </span>
+        <span class="player-search-link player-search-link-active" id="3">3 </span>
+        <span class="player-search-link player-search-link-active" id="4">4 </span>
+        <span class="player-search-link player-search-link-active" id="4">&gt;&gt;</span>
+    </div>
+    
+    <div class="events-list-container">
+    <h2>Events List</h2>
+    <table class='events-list-table'>
+	      <tr>
+	      <th>Player</th>
+	      <th>Date</th>
+	      <th>Type</th>
+          <th>Region</th>
+	      </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+          <tr>
+          <td>EventName</td>
+          <td>04/04/2019</td>
+          <td>Single</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+        
+        </table>
+    
+  </div>
+    
+    <div class="search-pagination-buttons">
+        <span class="player-search-link player-search-link-active" id="1">&lt;&lt;</span>
+        <span class="player-search-link player-search-link-active" id="1">1 </span>
+        <span class="player-search-link player-search-link-active" id="2">2 </span>
+        <span class="player-search-link player-search-link-active" id="3">3 </span>
+        <span class="player-search-link player-search-link-active" id="4">4 </span>
+        <span class="player-search-link player-search-link-active" id="4">&gt;&gt;</span>
+    </div>
+
+
+</article>
+
+<?php
+    include("./includes/footer.php");
+?>
--- clubs.php
+++ clubs.php
@@ -5,9 +5,123 @@
     include("./includes/navigation.php");
 ?>
 
-<article>
+<article id="club-page-article">
+
+    <div class="club-search-filter-container">
+
+        
+        <div id="club-search-filter-line">
+            <h1 id="club-search-filter-title">Search for a Club</h1>
+        </div>
+        
+        <div class="search-box">
+            <div class="search-field">
+            <input type="txt" class="search-input" placeholder="Search club">
+            <button class="search-button" type="button">Search</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="search-result-container">
+        <table class='search-result-table'>
+	      <tr> 
+	      <th>Club</th>
+	      <th>Sport</th>
+	      <th>Region</th>
+	      </tr>
+          <tr>
+          <td><a id='player-name-link' href="./club-profile.php">Habart Badminton Club</a></td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+            <tr>
+          <td>ClubName</td>
+          <td>Badminton</td>
+          <td>Australia, Tasmania</td>
+          </tr>
+        </table>
+    </div>
+    <div class="search-pagination-buttons">
+        <span class="player-search-link player-search-link-active" id="1">&lt;&lt;</span>
+        <span class="player-search-link player-search-link-active" id="1">1 </span>
+        <span class="player-search-link player-search-link-active" id="2">2 </span>
+        <span class="player-search-link player-search-link-active" id="3">3 </span>
+        <span class="player-search-link player-search-link-active" id="4">4 </span>
+        <span class="player-search-link player-search-link-active" id="4">&gt;&gt;</span>
+    </div>
 
 </article>
+
 
 <?php
     include("./includes/footer.php");
