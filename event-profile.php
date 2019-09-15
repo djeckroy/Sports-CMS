15da49bee94b16ed9b6a804e7f5f7deecd7623df
Mingxinwen <Mingxinwen@users.noreply.github.com>
2019-09-15 20:20:00 +1000

    Update on eventProfile Page

--- event-profile.php
+++ event-profile.php
@@ -1,149 +1,120 @@
-<?php 
-    $title = "Peterman Ratings | Event-profile";
+<?php
+$title = "Peterman Ratings | Event-profile";
 
-    include("./includes/header.php");
-    include("./includes/navigation.php");
+include( "./includes/header.php" );
+include( "./includes/navigation.php" );
 ?>
 
 <article id="event-profile-page-article">
-  <div class="events-information-container">
-      <h1>2019 Caboolture JDS- Sunshine Coast</h1>
-      <h2>ABC Tennis club</h2>
-      <h2>Single</h2>
-      <h2>07/06/2019</h2>
-      <h2>Tasmania</h2>
-      <h2>Australia</h2> 
-  </div>
-  <div class="matches-table-container">
-      <h2>Matches Overview</h2>
-      <table class='matches-table'>
-	      <tr>
-	      <th>Ranking</th>
-	      <th>Winner</th>
-          <th> </th>
-	      <th>Loser</th>
-	      <th>Ranking</th>
-	      </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          <tr>
-          <td>1234</td>
-          <td>PlayerName</td>
-          <td>-</td>
-          <td>LoserName</td>
-          <td>4567</td>
-          </tr>
-          
-        </table>
-    
-  </div>
-  <div class="search-pagination-buttons">
-        <span class="player-search-link player-search-link-active" id="1">&lt;&lt;</span>
-        <span class="player-search-link player-search-link-active" id="1">1 </span>
-        <span class="player-search-link player-search-link-active" id="2">2 </span>
-        <span class="player-search-link player-search-link-active" id="3">3 </span>
-        <span class="player-search-link player-search-link-active" id="4">4 </span>
-        <span class="player-search-link player-search-link-active" id="4">&gt;&gt;</span>
-    </div>
+	<div class="events-information-container">
+		<h1>2019 Caboolture JDS- Sunshine Coast</h1>
+		<h2>ABC Tennis club</h2>
+		<h2>Single</h2>
+		<h2>07/06/2019</h2>
+		<h2>Tasmania</h2>
+		<h2>Australia</h2>
+	</div>
+
+	<div class="matches-table-container">
+		<table class='matches-table'>
+			<tr>
+				<th> </th>
+				<th><a id='player-link' href="/player/profile">Bailey Fred</a>
+				</th>
+				<th id="doubleDefeat">defeat</th>
+				<th><a id='player-link' href="/player/profile">Collins George</a>
+				</th>
+				<th> </th>
+			</tr>
+			<tr>
+
+				<tr>
+					<th> </th>
+					<th><a id='player-link' href="/player/profile">Clark Ernest</a>
+					</th>
+					<th> </th>
+					<th><a id='player-link' href="/player/profile">Butler Alan</a>
+					</th>
+				</tr>
+				<tr>
+
+					<tr>
+						<td class="strong">Previous Ranking:</td>
+						<td>4326</td>
+						<td>-</td>
+						<td>3250</td>
+					</tr>
+					<tr>
+						<td class="strong">Ranking Change:</td>
+						<td>+100</td>
+						<td>-</td>
+						<td>-50</td>
+					</tr>
+					<tr>
+						<td class="strong">New Ranking:</td>
+						<td>4426</td>
+						<td>-</td>
+						<td>3200</td>
+					</tr>
+					<tr>
+						<td class="strong">Set Score:</td>
+						<td>426</td>
+						<td>-</td>
+						<td>332</td>
+					</tr>
+		</table>
+	</div>
+
+	<div class="matches-table-container">
+		<table class='matches-table'>
+			<tr>
+				<th> </th>
+				<th><a id='player-link' href="/player/profile">Bailey Fred</a>
+				</th>
+				<th>defeats</th>
+				<th><a id='player-link' href="/player/profile">Collins George</a>
+				</th>
+			</tr>
+			<tr>
+
+				<tr>
+					<td class="strong">Previous Ranking:</td>
+					<td>4326</td>
+					<td>-</td>
+					<td>3250</td>
+				</tr>
+				<tr>
+					<td class="strong">Ranking Change:</td>
+					<td>+100</td>
+					<td>-</td>
+					<td>-50</td>
+				</tr>
+				<tr>
+					<td class="strong">New Ranking:</td>
+					<td>4426</td>
+					<td>-</td>
+					<td>3200</td>
+				</tr>
+				<tr>
+					<td class="strong">Set Score:</td>
+					<td>426</td>
+					<td>-</td>
+					<td>332</td>
+				</tr>
+		</table>
+	</div>
+
+	<div class="search-pagination-buttons">
+		<span class="player-search-link player-search-link-active" id="1">&lt;&lt;</span>
+		<span class="player-search-link player-search-link-active" id="1">1 </span>
+		<span class="player-search-link player-search-link-active" id="2">2 </span>
+		<span class="player-search-link player-search-link-active" id="3">3 </span>
+		<span class="player-search-link player-search-link-active" id="4">4 </span>
+		<span class="player-search-link player-search-link-active" id="4">&gt;&gt;</span>
+	</div>
 
 </article>
 
 <?php
-    include("./includes/footer.php");
+include( "./includes/footer.php" );
 ?>
