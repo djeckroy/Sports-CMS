ae02a19ea552f865dd32f62eeef25c766d5057f4
Adib-shadman <40189935+Adib-shadman@users.noreply.github.com>
2019-10-09 13:11:21 +1100

    Added BAppSc 2019 in the footer

--- includes/footer.php
+++ includes/footer.php
@@ -30,7 +30,7 @@
 
     <div class="footer-bottom-content">
       <hr/>
-      <small>&copy; <?php echo date('Y'); ?> Peter Hadrill 2019. All Rights Reserved.</small>
+      <small>&copy; Peter Hadrill BAppSc <?php echo date('Y'); ?> . All Rights Reserved.</small>
     </div>
 
   </div>
